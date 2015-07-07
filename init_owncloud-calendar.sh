1d3d02e5c4cd369c0b08bca78b418446ff6388db
Niklas Terhoeven <niklas@terhoeven.me>
2015-07-07 22:00:40 +0200

    used variables for commands

--- init_owncloud-calendar.sh
+++ init_owncloud-calendar.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-syncevolution --configure database=https://meinserver.com/owncloud/remote.php/caldav/calendars/MYUSER/pers%C3%B6nlich backend=caldav target-config@owncloud calendar
-syncevolution --configure sync=two-way backend=events database="Persönlich" owncloud calendar
+CALURL=$1
+CALNAME=$2
+
+# These commands are copied from http://wiki.ubuntuusers.de/Evolution/Synchronisation
+syncevolution --configure database="$CALURL" backend=caldav target-config@owncloud calendar
+syncevolution --configure sync=two-way backend=events database="$CALNAME" owncloud calendar
 syncevolution --sync slow owncloud calendar
