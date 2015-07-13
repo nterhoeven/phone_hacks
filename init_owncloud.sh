d6d3897ce6c8396fc47592eb25180df686b3e629
Niklas Terhoeven <niklas@terhoeven.me>
2015-07-14 01:00:33 +0200

    added SSLVerifyServer="0" found at https://forum.ubuntuusers.de/topic/ubuntu-touch-mit-owncloud-synchronisieren/

--- init_owncloud.sh
+++ init_owncloud.sh
@@ -5,5 +5,6 @@ OCPASS=
 OCURL=
 
 # These commands are copied from http://wiki.ubuntuusers.de/Evolution/Synchronisation
-syncevolution --configure --keyring=no --template webdav username="$OCUSER" password="$OCPASS" syncurl="$OCURL" target-config@owncloud
+syncevolution --configure --keyring=no --template webdav username="$OCUSER" password="$OCPASS" syncurl="$OCURL" SSLVerifyServer="0" target-config@owncloud
 syncevolution --configure --template SyncEvolution_Client sync=none syncURL=local://@owncloud username= password= peerIsClient=1 owncloud
+
