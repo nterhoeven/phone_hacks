2a41fd6bd6b306e929870d34c0b60b82dbe523ef
Niklas Terhoeven <niklas@terhoeven.me>
2015-07-14 00:14:55 +0200

    added keyring=no option to aviod gnome keyring error

--- init_owncloud.sh
+++ init_owncloud.sh
@@ -5,5 +5,5 @@ OCPASS=
 OCURL=
 
 # These commands are copied from http://wiki.ubuntuusers.de/Evolution/Synchronisation
-syncevolution --configure --template webdav username="$OCUSER" password="$OCPASS" syncurl="$OCURL" target-config@owncloud
+syncevolution --configure --keyring=no --template webdav username="$OCUSER" password="$OCPASS" syncurl="$OCURL" target-config@owncloud
 syncevolution --configure --template SyncEvolution_Client sync=none syncURL=local://@owncloud username= password= peerIsClient=1 owncloud
