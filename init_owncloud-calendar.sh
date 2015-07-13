8331f32a678c6d41270efe947b0212278d981dcc
Niklas Terhoeven <niklas@terhoeven.me>
2015-07-14 01:05:40 +0200

    added commands from https://forum.ubuntuusers.de/topic/ubuntu-touch-mit-owncloud-synchronisieren/

--- init_owncloud-calendar.sh
+++ init_owncloud-calendar.sh
@@ -10,7 +10,13 @@ CALNAME=$2
 
 
 # These commands are from https://help.ubuntu.com/community/SyncEvolution/Synchronize-evolution-data-with-caldav-cardav-server
-syncevolution --configure backend=evolution-calendar database="$CALNAME" owncloud calendar
-syncevolution --configure database="$CALURL" backend=caldav target-config@owncloud calendar
-syncevolution --configure sync=two-way database="$CALNAME" owncloud calendar
-syncevolution --sync refresh-from-remote owncloud calendar
+# syncevolution --configure backend=evolution-calendar database="$CALNAME" owncloud calendar
+# syncevolution --configure database="$CALURL" backend=caldav target-config@owncloud calendar
+# syncevolution --configure sync=two-way database="$CALNAME" owncloud calendar
+# syncevolution --sync refresh-from-remote owncloud calendar
+
+# Maybe these commands from https://forum.ubuntuusers.de/topic/ubuntu-touch-mit-owncloud-synchronisieren/ will work
+syncevolution --configure --template webdav database="$CALURL" backend=caldav target-config@owncloud "$CALNAME"
+syncevolution --create-database backend=calendar database="$CALNAME"
+syncevolution --configure sync=two-way backend=calendar database="$CALNAME" owncloud "$CALNAME"
+syncevolution --sync slow owncloud "$CALNAME"
