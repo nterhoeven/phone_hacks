03f44701a60e09d16ffb213522e1f348de1ffe4f
Niklas Terhoeven <niklas@terhoeven.me>
2015-07-14 00:21:31 +0200

    added commands from a different tutorial - maybe these will work

--- init_owncloud-calendar.sh
+++ init_owncloud-calendar.sh
@@ -4,6 +4,13 @@ CALURL=$1
 CALNAME=$2
 
 # These commands are copied from http://wiki.ubuntuusers.de/Evolution/Synchronisation
+# syncevolution --configure database="$CALURL" backend=caldav target-config@owncloud calendar
+# syncevolution --configure sync=two-way backend=events database="$CALNAME" owncloud calendar
+# syncevolution --sync slow owncloud calendar
+
+
+# These commands are from https://help.ubuntu.com/community/SyncEvolution/Synchronize-evolution-data-with-caldav-cardav-server
+syncevolution --configure backend=evolution-calendar database="$CALNAME" owncloud calendar
 syncevolution --configure database="$CALURL" backend=caldav target-config@owncloud calendar
-syncevolution --configure sync=two-way backend=events database="$CALNAME" owncloud calendar
-syncevolution --sync slow owncloud calendar
+syncevolution --configure sync=two-way database="$CALNAME" owncloud calendar
+syncevolution --sync refresh-from-remote owncloud calendar
